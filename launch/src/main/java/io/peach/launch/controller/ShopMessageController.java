9c746bd902e0279678351346254789f7ad27a9c4
zixing <zinianly@163.com>
2019-09-19 14:56:03 +0800

    no message

--- launch/src/main/java/io/peach/launch/controller/ShopMessageController.java
+++ launch/src/main/java/io/peach/launch/controller/ShopMessageController.java
@@ -1,4 +1,5 @@
 package io.peach.launch.controller;
+import io.peach.launch.base.core.Constants;
 import io.peach.launch.base.core.Result;
 import io.peach.launch.base.core.ResultGenerator;
 import io.peach.launch.dto.SubmitAll;
@@ -15,9 +16,11 @@ import tk.mybatis.mapper.entity.Example;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 /**
 * Created by anshi on 2019/09/11.
@@ -74,26 +77,59 @@ public class ShopMessageController {
         return ResultGenerator.successResult(page);
     }
 
+    protected static String getDate() {
+        Date currentTime = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+        return simpleDateFormat.format(currentTime);
+    }
+
     @PostMapping("/uploadPicture")
-    public Result uploadPicture(HttpServletRequest request) {
-         /*获取上传的文件*/
-        MultipartHttpServletRequest req =(MultipartHttpServletRequest)request;
-        MultipartFile multipartFile =  req.getFile("image");
-        String path="";
-        /*判断当前文件是否为空  如果不为空上传文件  如果为空存放默认图片*/
-        if(multipartFile.getSize()>0){
-            String uploadPath="/static/"+multipartFile.getOriginalFilename();
-            File target=new File(uploadPath);
-            try {
-                multipartFile.transferTo(target);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            path=uploadPath;
-        }else{
-            path="/static/index.jpg";
+    public Result uploadPicture(HttpServletRequest request) throws Exception {
+
+        request.setCharacterEncoding("utf-8");  //设置编码
+
+        String randomName = UUID.randomUUID().toString() + ".jpg";
+        String imageName = getDate() + "/" + randomName;
+        //服务器真正存放的路径
+        String fileName = Constants.PATH_IMAGE_PATH + imageName;
+        // 保存图片到本地
+        File file = new File(fileName);
+        if (!file.getParentFile().exists()) {
+            file.getParentFile().mkdirs();
         }
-        return ResultGenerator.successResult(path);
+
+        OutputStream out = new FileOutputStream(file);
+        InputStream in = request.getPart("image").getInputStream();
+        int length = 0;
+        byte[] buf = new byte[1024];
+//             in.read(buf); 每次读到的数据存放在buf 数组中
+        while ((length = in.read(buf)) != -1) {
+//                //在buf数组中取出数据写到（输出流）磁盘上
+            out.write(buf, 0, length);
+        }
+        in.close();
+        out.close();
+
+        return ResultGenerator.successResult(fileName);
+//
+//         /*获取上传的文件*/
+//        MultipartHttpServletRequest req =(MultipartHttpServletRequest)request;
+//        MultipartFile multipartFile =  req.getFile("image");
+//        String path="";
+//        /*判断当前文件是否为空  如果不为空上传文件  如果为空存放默认图片*/
+//        if(multipartFile.getSize()>0){
+//            String uploadPath="/static/"+multipartFile.getOriginalFilename();
+//            File target=new File(uploadPath);
+//            try {
+//                multipartFile.transferTo(target);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//            path=uploadPath;
+//        }else{
+//            path="/static/index.jpg";
+//        }
+//        return ResultGenerator.successResult(path);
     }
 
     @PostMapping("/saveUserAndShopMessageAndGrading")
--- taro-mp/qzx/src/pages/mine/mine.tsx
+++ taro-mp/qzx/src/pages/mine/mine.tsx
@@ -1,39 +1,42 @@
 import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import {View, Text, Picker} from '@tarojs/components'
 import './mine.scss'
-import {AtImagePicker} from "taro-ui";
+import {AtButton, AtForm, AtImagePicker, AtInput} from "taro-ui";
+import {JSONSchema4} from "json-schema";
+import {CITY_DATA} from "../../constants/api";
 
 export default class Mine extends Component {
 
   constructor () {
     super(...arguments)
     this.state = {
-      files: [{
-        url: 'https://jimczj.gitee.io/lazyrepay/aragaki1.jpeg',
-      },
-        {
-          url: 'https://jimczj.gitee.io/lazyrepay/aragaki2.jpeg',
-        },
-        {
-          url: 'https://jimczj.gitee.io/lazyrepay/aragaki3.png',
-        }]
+      files: [],
+      selector: [['美国', '中国', '巴西', '日本'],['河南', '西藏', '北京'], ['郑州','拉萨','朝阳']],
+      selectorChecked: '美国'
     }
+    this.initData();
+  }
+
+  initData() {
+    Taro.request({
+      url: CITY_DATA
+    }).then(res => {
+      console.log(res);
+    })
   }
   onChange (files) {
     console.log(files);
-    // console.log(FileSystemManager.readFileSync(files[3].url, 'base64').length)
-    FileSystemManager.readFile({
-      filePath: files[3].url,
-      encoding: 'base64',
-      success: (res) => {
-        console.log(res);
-      },
-      fail: (err) => {
-        console.log(err);
-      },
-      complete: () => {
-        console.log('end');
-      }
+    Taro.uploadFile({
+      url: 'http://localhost:8081/shop/message/uploadPicture',
+      filePath: files[0].url,
+      name: 'image'
+    }).then((res) => {
+      console.log(res['data']);
+      console.log(typeof res['data']);
+      let tmp = JSON.parse(res['data']);
+      this.setState({
+        image: tmp['data']
+      })
     });
     this.setState({
       files
@@ -44,6 +47,86 @@ export default class Mine extends Component {
   }
   onImageClick (index, file) {
     console.log(index, file)
+  }
+
+  /*
+  owner_phone
+  shoptype_id
+  shopname
+  shoppicture
+  shopaddress
+
+  username
+  password
+   */
+
+  handleUsernameChange (username) {
+    this.setState({
+      username
+    })
+  }
+
+  handlePasswordChange (password) {
+    this.setState({
+      password
+    })
+  }
+
+  handlePhoneChange(phone) {
+    this.setState({
+      phone
+    })
+  }
+
+  handleShopNameChange(shopName) {
+    this.setState({
+      shopName
+    })
+  }
+
+  handleChange(value){
+    this.setState({
+      value
+    })
+  }
+
+  onSubmit (event) {
+    console.log(this.state);
+    console.log(event)
+    Taro.request({
+      url: 'http://localhost:8081/shop/message/saveUserAndShopMessageAndGrading',
+      data: {
+        user: {
+          username: this.state.username,
+          password: this.state.password
+        },
+        shopMessage: {
+          owner_phone: this.state.phone,
+          shoptype_id: '',
+          shopname: '',
+          shoppicture: ''
+        },
+        recommendID: 1,
+        positionID: 2
+      },
+      method: "POST",
+      header: {
+        'content-type': 'application/json'
+      }
+    }).then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+  onReset (event) {
+    console.log(event)
+  }
+
+  onAddressChange = e => {
+    this.setState({
+      selectorChecked: this.state.selector[e.detail.value]
+    })
   }
 
   /**
@@ -78,6 +161,89 @@ export default class Mine extends Component {
           files={this.state.files}
           onChange={this.onChange.bind(this)}
         />
+
+        <AtForm
+          onSubmit={this.onSubmit.bind(this)}
+          onReset={this.onReset.bind(this)}
+        >
+          <AtInput
+            name='value'
+            title='用户名'
+            type='text'
+            placeholder='单行文本'
+            value={this.state.username}
+            onChange={this.handleUsernameChange.bind(this)}
+          />
+
+          <AtInput
+            name='value3'
+            title='密码'
+            type='password'
+            placeholder='密码不能少于10位数'
+            value={this.state.password}
+            onChange={this.handlePasswordChange.bind(this)}
+          />
+
+          <AtInput
+            name='value6'
+            border={false}
+            title='手机号码'
+            type='phone'
+            placeholder='手机号码'
+            value={this.state.phone}
+            onChange={this.handlePhoneChange.bind(this)}
+          />
+
+          <AtInput
+            name='value'
+            title='店铺名'
+            type='text'
+            placeholder='单行文本'
+            value={this.state.shopName}
+            onChange={this.handleShopNameChange.bind(this)}
+          />
+
+          <View className='page-section'>
+            <Text>地区选择器</Text>
+            <View>
+              <Picker mode='multiSelector' range={this.state.selector} onChange={this.onAddressChange}>
+                <View className='picker'>
+                  当前选择：{this.state.selectorChecked}
+                </View>
+              </Picker>
+            </View>
+          </View>
+
+          {/*<AtInput*/}
+            {/*name='value2'*/}
+            {/*title='数字'*/}
+            {/*type='number'*/}
+            {/*placeholder='请输入数字'*/}
+            {/*value={this.state.value2}*/}
+            {/*onChange={this.handleChange.bind(this)}*/}
+          {/*/>*/}
+
+          {/*<AtInput*/}
+            {/*name='value4'*/}
+            {/*title='身份证'*/}
+            {/*type='idcard'*/}
+            {/*placeholder='身份证号码'*/}
+            {/*value={this.state.value4}*/}
+            {/*onChange={this.handleChange.bind(this)}*/}
+          {/*/>*/}
+          {/*<AtInput*/}
+            {/*name='value5'*/}
+            {/*title='小数'*/}
+            {/*type='digit'*/}
+            {/*placeholder='请输入小数'*/}
+            {/*value={this.state.value5}*/}
+            {/*onChange={this.handleChange.bind(this)}*/}
+          {/*/>*/}
+
+
+          <AtButton formType='submit'>提交</AtButton>
+          <AtButton formType='reset'>重置</AtButton>
+        </AtForm>
 
         {/*<SwNav/>*/}
       </View>
