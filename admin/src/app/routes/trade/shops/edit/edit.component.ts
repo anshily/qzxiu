7ad829324a77dc4c1052fb06da23cbfddea27359
zixing <zinianly@163.com>
2019-09-28 22:58:19 +0800

    no message

--- admin/src/app/routes/trade/shops/edit/edit.component.ts
+++ admin/src/app/routes/trade/shops/edit/edit.component.ts
@@ -10,6 +10,7 @@ import {SFSchema, SFSelectWidgetSchema, SFUISchema, SFUploadWidgetSchema} from '
 export class TradeShopsEditComponent implements OnInit {
   record: any = {};
   recommendList = [];
+  receiveContent: 'aaaa';
   i: any;
   schema: SFSchema = {
     properties: {
@@ -77,6 +78,11 @@ export class TradeShopsEditComponent implements OnInit {
     },
   };
 
+  updateContent(e){
+    console.log(e)
+    this.receiveContent = e;
+  }
+
   constructor(
     private modal: NzModalRef,
     private msgSrv: NzMessageService,
@@ -95,8 +101,72 @@ export class TradeShopsEditComponent implements OnInit {
     this.http.get(ROOT_URL + `shop/message/getRecommendAndPosition`).subscribe(res => {
       if (res['code'] == 0) {
         this.recommendList = res['data'];
+        let reList = []
+        this.recommendList.forEach(item => {
+          reList.push({
+            label: item['shopname'],
+            value: item['id']
+          })
+        })
+
+        this.schema = {
+          properties: {
+            username: { type: 'string', title: '用户' },
+            password: { type: 'string', title: '密码' },
+            phone: { type: 'string', title: '电话' },
+            shopName: { type: 'string', title: '店铺名', maxLength: 15 },
+            shopAddress: { type: 'string', title: '地址', maxLength: 15 },
+            shopType: { type: 'number', title: '店铺类型',enum: [
+                { label: '省代理', value: 1 },
+                { label: '市代理', value: 2 },
+                { label: '县代理', value: 3 },
+              ],
+              default: 1,
+              ui: {
+                widget: 'select',
+              } as SFSelectWidgetSchema },
+            recommendID: { type: 'number', title: '推荐人', enum: [
+                ...reList
+              ],
+              default: reList[0].value,
+              ui: {
+                widget: 'select',
+              } as SFSelectWidgetSchema },
+            positionID: { type: 'number', title: '代理人', enum: [
+                ...reList
+              ],
+              default: reList[0].value,
+              ui: {
+                widget: 'select',
+              } as SFSelectWidgetSchema },
+            file: {
+              type: 'string',
+              title: '门面图',
+              ui: {
+                widget: 'upload',
+                action: ROOT_URL + 'shop/message/uploadPicture',
+                resReName: 'data',
+                urlReName: 'url',
+                fileType: 'image/png,image/jpeg,image/gif,image/bmp',
+                name: 'image'
+              } as SFUploadWidgetSchema,
+            },
+            custom: {
+              type: 'string',
+              title: '描述',
+              ui: {
+                widget: 'custom',
+                grid: { span: 24 }
+              },
+              default: 'test',
+            }
+          },
+          required: ['username', 'password', 'shopName', 'shopAddress', 'shopType', 'recommendID', 'positionID', 'file', 'phone'],
+        };
+
       }
     });
+
   }
 
   save(value: any) {
@@ -110,7 +180,8 @@ export class TradeShopsEditComponent implements OnInit {
         shoptype_id: value.shopType,
         shopname: value.shopName,
         shoppicture: value.file,
-        shopaddress: value.shopAddress
+        shopaddress: value.shopAddress,
+        description: this.receiveContent
       },
       recommendID: value.recommendID,
       positionID: value.positionID
