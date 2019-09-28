559e0e0f7a3cb2fa6faf2ec073210bdc24b5cb4a
zixing <zinianly@163.com>
2019-09-29 00:20:49 +0800

    no message

--- taro-redux/src/pages/referrer/referrer.js
+++ taro-redux/src/pages/referrer/referrer.js
@@ -2,21 +2,35 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
 import { connect } from '@tarojs/redux'
+import { AtSteps } from 'taro-ui'
 import './referrer.scss'
 import * as actions from '@actions/referrer';
 
 @connect(state => state.referrerItem, actions)
 export default class Referrer extends Component {
+  onChange (current) {
+    this.setState({
+      current
+    })
+  }
+  state = {
+    current: 0,
+  }
   config = {
     navigationBarTitleText: 'referrer'
   }
   render() {
-    return(
-      <View>
-        <Text>
-          referrer
-        </Text>
-      </View>
+    const items = [
+      { 'title': '步骤一', 'desc': '这里是额外的信息，最多两行' },
+      { 'title': '步骤二', 'desc': '这里是额外的信息，最多两行' },
+      { 'title': '步骤三', 'desc': '这里是额外的信息，最多两行' }
+    ]
+    return (
+      <AtSteps
+        items={items}
+        current={this.state.current}
+        onChange={this.onChange.bind(this)}
+      />
     )
   }
 }
--- taro-redux/src/pages/user/menu/index.js
+++ taro-redux/src/pages/user/menu/index.js
@@ -13,50 +13,52 @@ const MENU_LIST = [{
   text: '添加商铺',
   img: require('./assets/pin.png')
 }, {
-  key: 'my-referrer',
+  key: 'referrer',
   text: '客户关系',
   img: require('./assets/bargain.png')
-}, {
-  key: 'credit',
-  text: '我的积分',
-  img: require('./assets/credit.png')
-}, {
-  key: 'service',
-  text: '退换/售后',
-  img: require('./assets/service.png')
-}, {
-  key: 'coupon',
-  text: '优惠券',
-  img: require('./assets/coupon.png')
-}, {
-  key: 'red-packet',
-  text: '红包',
-  img: require('./assets/red-packet.png')
-}, {
-  key: 'allowance',
-  text: '津贴',
-  img: require('./assets/allowance.png')
-}, {
-  key: 'gif-card',
-  text: '礼品卡',
-  img: require('./assets/gif-card.png')
-}, {
-  key: 'location',
-  text: '地址管理',
-  img: require('./assets/location.png')
-}, {
-  key: 'safe',
-  text: '账号安全',
-  img: require('./assets/safe.png')
-}, {
-  key: 'contact',
-  text: '联系客服',
-  img: require('./assets/contact.png')
-}, {
-  key: 'feedback',
-  text: '用户反馈',
-  img: require('./assets/feedback.png')
-}, {
+},
+//   {
+//   key: 'credit',
+//   text: '我的积分',
+//   img: require('./assets/credit.png')
+// }, {
+//   key: 'service',
+//   text: '退换/售后',
+//   img: require('./assets/service.png')
+// }, {
+//   key: 'coupon',
+//   text: '优惠券',
+//   img: require('./assets/coupon.png')
+// }, {
+//   key: 'red-packet',
+//   text: '红包',
+//   img: require('./assets/red-packet.png')
+// }, {
+//   key: 'allowance',
+//   text: '津贴',
+//   img: require('./assets/allowance.png')
+// }, {
+//   key: 'gif-card',
+//   text: '礼品卡',
+//   img: require('./assets/gif-card.png')
+// }, {
+//   key: 'location',
+//   text: '地址管理',
+//   img: require('./assets/location.png')
+// }, {
+//   key: 'safe',
+//   text: '账号安全',
+//   img: require('./assets/safe.png')
+// }, {
+//   key: 'contact',
+//   text: '联系客服',
+//   img: require('./assets/contact.png')
+// }, {
+//   key: 'feedback',
+//   text: '用户反馈',
+//   img: require('./assets/feedback.png')
+// },
+  {
   key: 'help',
   text: '帮助中心',
   url: 'http://m.you.163.com/help',
@@ -73,9 +75,9 @@ export default class Menu extends Component {
       Taro.navigateTo({
         url: '/pages/enter/enter'
       })
-    } else if (menu.key === 'my-referrer') {
+    } else if (menu.key === 'referrer') {
       Taro.navigateTo({
-        url: '/pages/my-referrer/my-referrer'
+        url: '/pages/referrer/referrer'
       })
     } else {
       Taro.showToast({
