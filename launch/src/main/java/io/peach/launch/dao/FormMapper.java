4f7046876560ff88371ec18cd9f96414a3f5fbcd
pigshit <1037497262@qq.com>
2020-01-05 17:17:20 +0800

    11

--- launch/src/main/java/io/peach/launch/dao/FormMapper.java
+++ launch/src/main/java/io/peach/launch/dao/FormMapper.java
@@ -3,5 +3,8 @@ package io.peach.launch.dao;
 import io.peach.launch.base.core.Mapper;
 import io.peach.launch.model.Form;
 
+import java.util.List;
+
 public interface FormMapper extends Mapper<Form> {
+    List<Form> getFormByStatu(int statu);
 }
--- launch/src/main/java/io/peach/launch/service/FormService.java
+++ launch/src/main/java/io/peach/launch/service/FormService.java
@@ -2,10 +2,13 @@ package io.peach.launch.service;
 import io.peach.launch.model.Form;
 import io.peach.launch.base.core.Service;
 
+import java.util.List;
+
 
 /**
  * Created by anshi on 2019/12/31.
  */
 public interface FormService extends Service<Form> {
 
+    List<Form> getFormByStatu(int statu);
 }
